306e7e0b2acb8958fa2d2688ae4bf70a3d052b49
ZeroMurder <lordlordsupercell666@gmail.com>
2026-04-19 11:27:11 +0300

    THIS PROJECT IS NO REAL ATTACK

---
+++ RiderHex.cpp
@@ -0,0 +1,110 @@
+
+//This learning 2.
+#include <iostream>
+//#include <Windows.h>
+//using namespace std;
+//
+//void number(int, int);
+//int main() {
+//	SetConsoleCP(1251);
+//	number(2, 7);
+//	number(8, 9);
+//
+//	system("pause");
+//	return 0;
+//}
+//
+//void number(int a, int b) {
+//	if (a <= b)
+//		while (a <= b)
+//			cout << a++ << " ";
+//    else {
+//		cout << "Error" << endl;
+//	}
+//}
+//class Calculator {
+//public:
+//    void show_menu() { /* TODO */ }
+//    double add(double a, double b) { return a + b; }
+//    double minus(double a, double b) { return a - b; }
+//    double addplus(double a, double b) { return a * b; }
+//    double minusplus(double a, double b) { return a / b; }
+//};
+//
+//int main() {
+//    Calculator calc;
+//    if (calc == 1) {
+//
+//    }
+//    calc.run();  // Бесконечный цикл меню
+//}
+
+//This code is calculator
+//#include <iostream>
+//#include <windows.h>
+//using namespace std;
+//
+//void add(int a, int b);
+//void minuss(int a, int b);
+//void addplus(int a, int b);
+//void minusminus(int a, int b);
+//
+//int main() {
+//	SetConsoleCP(1251);
+//	cout << "1 add, 2 minuss, 3 devide(minusminus), 4 multiple(addplus)" << endl;
+//	int choice;
+//	cin >> choice;
+//	int a, b;
+//	cout << "a , b :";
+//	cin >> a >> b;
+//
+//	if (choice == 1)add(a, b);
+//	else if (choice == 2)minuss(a, b);
+//	else if (choice == 3)addplus(a, b);
+//	else if (choice == 4)minusminus(a, b);
+//	else cout << "error" << endl;
+//
+//	system("pause");
+//	return 0;
+//}
+//	void add(int a, int b) {
+//		cout << a + b << endl;
+//	}	
+//	void minuss(int a, int b) {
+//		cout << a - b << endl;
+//	}	
+//	void addplus(int a, int b) {
+//		cout << a * b << endl;
+//	}	
+//	void minusminus(int a, int b) {
+//		if (b == 0) {
+//			cout << "Error minusminus 0" << endl;
+//		}
+//		else {
+//			cout << a / (double) b << endl;
+//		}
+//	}
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+void Ddos();
+
+int main() {
+	SetConsoleCP(1251);
+	int user;
+	void Ddos();
+	cout << "Connected User" << endl;
+	cin >> user;
+	if (user == 1) Ddos();
+	system("pause");
+	return 0;
+}
+
+void Ddos() {
+	cout << "ddos accept conet" << endl;
+}
+
+
+
+
